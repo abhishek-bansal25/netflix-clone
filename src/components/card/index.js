a85e98220d40cd94a88af9355672c3cf761c0fe7
abhishek basnal <abbansal25@gmail.com>
2021-09-24 11:19:02 +0530

    cards added

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -43,6 +43,30 @@ Card.Meta = function CardMeta({ children, ...restProps }) {
     return <Meta {...restProps}>{children}</Meta>;
 };
 
+Card.Feature = function CardFeature ({childern, category, ...restProps}){
+    const {showFeature, itemFeature, setShowFeature} = useContext(FeatureContext)
+
+    return showFeature ? (
+        <Feature {...restProps} src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`} >
+            <Content>
+                <FeatureTitle>{itemFeature.title}</FeatureTitle>
+                <FeatureText>{itemFeature.description}</FeatureText>
+                <FeatureClose onClick={()=> setShowFeature(false)}>
+                    <img src="images/icons/close.png" alt="close"/>
+                </FeatureClose>
+            </Content>
+            <Group margin="30px 0" flexDirection="row" alignItem="center">
+                <Maturity rating={itemFeature.maturity}>
+                    {itemFeature.maturity < 12 ? "PG" : itemFeature.maturity}
+                </Maturity>
+                <FeatureText fontWeight="bold">
+                    {itemFeature.genre.charAt(0).toUpperCase() + itemFeature.genre.slice(1)}
+                </FeatureText>
+            </Group>
+        </Feature>
+    ) : null ;
+}
+
 Card.Item = function CardItem({item, children, ...restProps}){
     const {setShowFeature, setItemfeature} = useContext(FeatureContext)
     
